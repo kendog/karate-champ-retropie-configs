023f1e466fe9e85b0d507cca4713c7abfc2f6431
Kenny Chen <kennychen@Kennys-MacBook-Pro.local>
2021-10-27 01:14:41 -0700

    first post

---
+++ opt/retropie/configs/all/autostart.sh
@@ -0,0 +1,2 @@
+# autostart karate champ && start emulation station if user exits
+/opt/retropie/supplementary/runcommand/runcommand.sh 0 _SYS_ arcade ~/RetroPie/roms/arcade/kchamp.zip && emulationstation #auto
